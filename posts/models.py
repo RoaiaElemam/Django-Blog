e9de5298682ebec3d6f08ac570a71979391d7e8a
RoaiaElemam <mroaia219@gmail.com>
2023-09-30 19:28:18 +0300

    design post model pt1

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+'''
+title
+author
+content
+image
+publish_date
+tags
+'''
+'''
+-fields
+-html
+-validation
+'''
+class post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(max_length=500000)
+    publish_date=models.DateTimeField()
+    
